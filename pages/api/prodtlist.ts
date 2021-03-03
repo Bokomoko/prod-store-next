448ddd83ebfc152de171b1cf9c820607e9673018
João Eurico <X45959790459@tcu.gov.br>
2021-03-03 18:13:49 -0300

    Trying to solve cors issue on vercel

--- pages/api/prodtlist.ts
+++ pages/api/prodtlist.ts
@@ -2,7 +2,8 @@ import { connectToDatabase } from "../../util/mongodb";
 
 export default async (req, res) => {
   const { db } = await connectToDatabase();
-
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
   const prodList = await db
     .collection("produtos")
     .find({})
