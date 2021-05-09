f5214c5b70aa83b7d1f786adb533e6658356a638
Bokomoko <bokomoko@gmail.com>
2021-05-09 17:44:24 -0300

    Product list can now be filtered by search string

--- pages/api/prodtlist.ts
+++ pages/api/prodtlist.ts
@@ -4,9 +4,16 @@ export default async (req, res) => {
   const { db } = await connectToDatabase();
   res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Origin', '*')
+  const { findWhat } = req
+  let findString = ""
+  if (findWhat) {
+    findString = `{ $text: { $search: "${findWhat}" } }`
+  }
+
+
   const prodList = await db
     .collection("produtos")
-    .find({})
+    .find({ findString })
     .limit(20)
     .toArray();
 
